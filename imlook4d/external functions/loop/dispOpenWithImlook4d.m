aa0ff9383a9be388c005afb40750121cbdb152df
Unknown <axelsson.jan@gmail.com>
2018-06-09 19:50:15 +0200

    Hyperlink print to workspace, to open file path with imlook4d

--- imlook4d/external functions/loop/dispOpenWithImlook4d.m
+++ imlook4d/external functions/loop/dispOpenWithImlook4d.m
@@ -1,2 +1,12 @@
-function dispOpenWithImlook4d( filePath)
-    disp( [ '<a href="matlab:imlook4d(''' filePath ''')">' filePath '</a>' ]);
+function dispOpenWithImlook4d( varargin )
+
+    text = '';
+    if nargin==1
+        filePath = varargin{1}
+    end
+    
+    if nargin==2
+        text = varargin{1};
+        filePath = varargin{2};
+    end
+    disp( [ text '<a href="matlab:imlook4d(''' filePath ''')">' filePath '</a>' ]);
