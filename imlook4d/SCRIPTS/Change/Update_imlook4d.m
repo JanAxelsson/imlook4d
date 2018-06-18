a1e37f5bc129cd6dd62ba6664a74b713d292b9b5
Unknown <axelsson.jan@gmail.com>
2018-06-18 20:27:15 +0200

    Update imlook4d -- get and install latest from Google Drive

--- imlook4d/SCRIPTS/Change/Update_imlook4d.m
+++ imlook4d/SCRIPTS/Change/Update_imlook4d.m
@@ -25,7 +25,11 @@ urlToLatestImlook4d = url;
 imlook4dFilePath = which('imlook4d.m');
 [folder,file,ext] = fileparts(imlook4dFilePath);
 zipFilePath = [ folder filesep '..' filesep 'latestImlook4d.zip'];
-unzipFolderPath = [ folder filesep '..' filesep 'latestImlook4d'];
+
+[zipFileFolder,file,ext] = fileparts(folder); % Same level as imlook4d folder
+zipFilePath = [ zipFileFolder filesep 'latestImlook4d.zip'];
+
+unzipFolderPath = [ zipFileFolder filesep 'latestImlook4d'];
 
 % TODO : url to latest path
 %urlToLatestImlook4d = 'https://drive.google.com/uc?export=download&id=12uG7-vkIFHpvWtMohuo3O-oszpjFGL98';
