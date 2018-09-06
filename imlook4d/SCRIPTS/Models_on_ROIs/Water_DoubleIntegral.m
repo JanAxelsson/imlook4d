143756fbd344d58a9ad7f991515b7fd8947a72b2
Unknown <axelsson.jan@gmail.com>
2018-09-06 22:33:49 +0200

    Water model on ROI -- use selected ref roi

--- imlook4d/SCRIPTS/Models_on_ROIs/Water_DoubleIntegral.m
+++ imlook4d/SCRIPTS/Models_on_ROIs/Water_DoubleIntegral.m
@@ -17,12 +17,12 @@ model_name = 'Water Double Integral Method';
     tacts = generateTACT(imlook4d_current_handles,imlook4d_ROI);  % ROIs
     ref = generateReferenceTACT( imlook4d_current_handles)
 
-    % Reference Region, mean of all ROIs
-    indecesToROI=find(imlook4d_ROI>0);  
-    for i=1:size(imlook4d_Cdata,4)
-        tempData = imlook4d_Cdata(:,:,:,i);
-        ref(i) = mean( tempData(indecesToROI) );
-    end
+%     % Reference Region, mean of all ROIs
+%     indecesToROI=find(imlook4d_ROI>0);  
+%     for i=1:size(imlook4d_Cdata,4)
+%         tempData = imlook4d_Cdata(:,:,:,i);
+%         ref(i) = mean( tempData(indecesToROI) );
+%     end
 
     tact = tacts;  % all ROIs
 
