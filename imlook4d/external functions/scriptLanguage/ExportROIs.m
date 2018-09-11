632feaf672776084bddeeb6dbb366da61887c922
jan <axelsson.jan@gmail.com>
2018-09-11 15:15:34 +0200

    Speed - ROI data to workspace

--- imlook4d/external functions/scriptLanguage/ExportROIs.m
+++ imlook4d/external functions/scriptLanguage/ExportROIs.m
@@ -73,10 +73,36 @@
     for i=1:lastROI
         disp([num2str(i) ') Evaluating ROI=' names{i}]);
         roiPixels=[]; % All pixel values in current ROI
+
+% SLOW old way:
+%        % loop frames
+%             for j=1:size(imlook4d_Cdata,4)
+%                 oneFrame=imlook4d_Cdata(:,:,:,j);
+%                 roiPixels(:,j)=oneFrame(imlook4d_ROI==i);
+%                 imlook4d_ROI_data.Npixels(i)=length(roiPixels(:,j));
+%             end        
+        
+% New way:
+        
+        % Determine indeces to Cdata matrix for this ROI
+        indecesToROI=find(imlook4d_ROI==i); % from ROI definition
+        NPixels(i)=size(indecesToROI,1);    % Number of pixels in ROI i
+        offset=size(imlook4d_ROI(:),1 );    % offset to next frame (number of pixels in volume)
+        allIndecesToROI=zeros(size(indecesToROI,1),numberOfFrames); % place for indeces in all frames
+        
+        % Get all indeces
+        for j=1:numberOfFrames
+            allIndecesToROI(:,j)=indecesToROI+(j-1)*offset;
+        end
+        
+        roiPixels = imlook4d_Cdata(allIndecesToROI);
+
+% Same as before:        
+        
         % loop frames
             for j=1:size(imlook4d_Cdata,4)
-                oneFrame=imlook4d_Cdata(:,:,:,j);
-                roiPixels(:,j)=oneFrame(imlook4d_ROI==i);
+%                 oneFrame=imlook4d_Cdata(:,:,:,j);
+%                 roiPixels(:,j)=oneFrame(imlook4d_ROI==i);
                 imlook4d_ROI_data.Npixels(i)=length(roiPixels(:,j));
             end
             
@@ -212,58 +238,3 @@
     
     
     ClearVariables
-%     disp('Exported to matrix imlook4d_ROI_data.mean    [ROI, frame]');
-%     disp('Exported to matrix imlook4d_ROI_data.std     [ROI, frame]');
-%     disp('Exported to matrix imlook4d_ROI_data.max     [ROI, frame]');
-%     disp('Exported to matrix imlook4d_ROI_data.min     [ROI, frame]');
-%     disp('Exported to cell   imlook4d_ROI_data.pixels{ROI}(pixel,frame)' );
-
-% 
-% GUIDANCE ON HOW TO CONTROL AN IMLOOK4D INSTANCE FROM SCRIPTS
-% ------------------------------------------------------------
-%  
-% WORK WITH IMLOOK4D INSTANCE IN MATLAB - THE SIMPLE WAY
-%
-%   1a) imlook4d/SCRIPT menu to make active window handles in workspace:
-%       imlook4d_current_handle     handle to imlook4d equivalent to hObject in imlook4d callback functions
-%       imlook4d_current_handles    equivalent to handles in this code (This is what you work with)
-%
-%   1b) imlook4d/Workspace/Export or Import menu to push active window data to workspace or pull from workspace:
-%
-%         This method applies to common data.  Otherwise methods 2) and 3)
-%         below must be used.
-%
-%         imlook4d_time        vector of frame times (exists only when time data exists)
-%         imlook4d_duration    vector of frame duration (exists only when duration data exists)
-%         imlook4d_Cdata       4D matrix of image data [x, y, slice,frame]
-%         imlook4d_ROI         3D matrix of ROI data [x, y, slice] where the pixel value equals the ROI number (thus, only one ROI possible in each pixel)
-%         imlook4d_ROINames    ROI names
-%
-%         NOT IMPORTED:  imlook4d_frame       current frame
-%         NOT IMPORTED:  imlook4d_slice       current slice
-%         NOT IMPORTED:  imlook4d_current_handle     handle to imlook4d equivalent to hObject in imlook4d callback functions
-%         NOT IMPORTED:  imlook4d_current_handles    equivalent to handles in this code (This is what you work with)
-%
-%   2) Example:  Modify handles.image.CachedImage from matlab workspace
-%       imlook4d_current_handles.image.CachedImage=1000;
-%
-%   3) Save changed handles to current Imlook4d instance.  This call
-%      attaches the modified imlook4d_current_handles to the current imlook4d
-%      instance: 
-%       guidata(imlook4d_current_handle, imlook4d_current_handles)
-%
-%
-% CALL FUNCTIONS FROM SCRIPT
-%
-% This applies to calling a function in the current imlook4d instance.
-% Relevant examples are calling the Export and Imports from workspace via
-% their respective menu callback functions.
-%
-% EXPORT from current imlook4d
-%    imlook4d('exportToWorkspace_Callback', imlook4d_current_handle,{},imlook4d_current_handles); % Exports to workspace from current imlook4d instance
-%
-% IMPORT to new imlook4d  
-%     tempHandle=imlook4d;  % New imlook4d instance
-%     tempHandles=guidata(tempHandle);
-%     imlook4d('importFromWorkspace_Callback', tempHandle,{},tempHandles);   % Import from workspace
-%
