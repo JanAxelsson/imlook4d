887b2dcbc1184fe06ee79c555affbfe22d474db5
jan <axelsson.jan@gmail.com>
2019-04-29 13:38:43 +0200

    Fix Flip error w PCA

--- imlook4d/SCRIPTS/requiredToolboxesForSCRIPTS.m
+++ imlook4d/SCRIPTS/requiredToolboxesForSCRIPTS.m
@@ -5,3 +5,4 @@ hash.ExampleScriptUsingAllDefinedToolBoxes = {'imagingToolbox', 'statisticalTool
 % Existing script dependencies
 hash.Flood_Fill_ROI = {'imagingToolbox'};
 hash.Flood_Fill_within_ROI = {'imagingToolbox'};
+hash.Register_with_Fiducial_Markers = {'imagingToolbox'};
