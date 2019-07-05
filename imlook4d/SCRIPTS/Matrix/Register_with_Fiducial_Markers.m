badbdcf17ee69cde8fa48cfb7d2c31c2f94dfe5a
jan <axelsson.jan@gmail.com>
2019-07-05 15:10:30 +0200

    First attempt fiducial markers

--- imlook4d/SCRIPTS/Matrix/Register_with_Fiducial_Markers.m
+++ imlook4d/SCRIPTS/Matrix/Register_with_Fiducial_Markers.m
@@ -4,54 +4,110 @@
 
 % Use current image as moving image
 ExportUntouched;
-B_dyn = imlook4d_Cdata; % Possibly 4D matrix
+B_4D = imlook4d_Cdata; % Possibly 4D matrix
+B_windowName = imlook4d_current_handles.figure1.Name;
+
+
+B_ROI = imlook4d_ROI; 
+B_ROI_names = imlook4d_ROINames;
+
+% ORIGINAL (called B)
 
 % Read fiducial markers for current image
-PB = getRoiCGs(imlook4d_ROI)';
-disp('Move from points :');
-disp([ [ 'x: ';'y: ';'z: '] num2str(PB) ]);
+PB0 = getRoiCGs(imlook4d_ROI)';
 sizeB = size(imlook4d_ROI);
-x = sizeB(1) * imlook4d_current_handles.image.pixelSizeX;
-y = sizeB(2) * imlook4d_current_handles.image.pixelSizeY;
-z = sizeB(3) * imlook4d_current_handles.image.sliceSpacing;
+x = 0.5 * sizeB(1) * imlook4d_current_handles.image.pixelSizeX;
+y = 0.5 * sizeB(2) * imlook4d_current_handles.image.pixelSizeY;
+z = 0.5 * sizeB(3) * imlook4d_current_handles.image.sliceSpacing;
 RB = imref3d(sizeB,[-x x],[-y y],[-z z]); %  real world coordinates
 
+% PB in world coordinates
+[xWorld, yWorld, zWorld] = intrinsicToWorld(RB,PB0(1,:)', PB0(2,:)',PB0(3,:)');
+PB = [ xWorld'; yWorld'; zWorld'];
+
+
+% TEMPLATE (called A)
 
 % Select template image (non-moving image)
 templateHandle = SelectWindow({'Select template image (from imlook4d/Windows menu)', '(image that we want slices to match'});
-imlook4d_current_handle = figure(templateHandle.Parent); % New window
+imlook4d_current_handle = figure(templateHandle.Parent); % Selected window
 
 % Read fiducial markers for template image
 ExportUntouched;
-PA = getRoiCGs(imlook4d_ROI)';
-disp('to points :');
-disp([ [ 'x: ';'y: ';'z: '] num2str(PA) ]);
+A_windowName = imlook4d_current_handles.figure1.Name;
+PA0 = getRoiCGs(imlook4d_ROI)';
+
 sizeA = size(imlook4d_ROI);
-x = sizeA(1) * imlook4d_current_handles.image.pixelSizeX;
-y = sizeA(2) * imlook4d_current_handles.image.pixelSizeY;
-z = sizeA(3) * imlook4d_current_handles.image.sliceSpacing;
+x = 0.5 * sizeA(1) * imlook4d_current_handles.image.pixelSizeX;
+y = 0.5 * sizeA(2) * imlook4d_current_handles.image.pixelSizeY;
+z = 0.5 * sizeA(3) * imlook4d_current_handles.image.sliceSpacing;
 RA = imref3d(sizeA,[-x x],[-y y],[-z z]); %  real world coordinates
 
+% PA in world coordinates
+[xWorld, yWorld, zWorld] = intrinsicToWorld( RB, PA0(1,:)', PA0(2,:)',PA0(3,:)');
+PA = [ xWorld'; yWorld'; zWorld'];
+
+
+% Print points
+disp([ 'Register window "' B_windowName ' :' ]);
+disp([ [ '  x: ';'  y: ';'  z: '] num2str(PB) ]);
+
+
+disp([ 'to window "' A_windowName ' :' ]);
+disp([ [ '  x: ';'  y: ';'  z: '] num2str(PA) ]);
+
+% % Graph points
+% figure;
+% scatter3( PB(1,:), PB(2,:), PB(3,:));
+% hold on;
+% p=PA;scatter3( PA(1,:), PA(2,:), PA(3,:));
+% for i = 1 : size( PB,2)
+%     line( [PB(1,i), PA(1,i)], [PB(2,i), PA(2,i)], [PB(3,i), PA(3,i)],'Color','black');
+% end
+% 
+% xlabel('x');ylabel('y');zlabel('z');
+% hold off;
+% legend('b','a');
 
 % Calculate Registration Parameters
-[regParams,Bfit,ErrorStats]=absor(PA,PB,'DoScale',1);
-T = regParams.M.';
+%[regParams,Bfit,ErrorStats]=absor(PA,PB,'DoScale',1);
+%[regParams,Bfit,ErrorStats]=absor(PA,PB,'doTrans',0);
 
-T = [regParams.s * regParams.R ,[0 0 0]';[0 0 0 1] ]'; % Ignore translation
+[regParams,Bfit,ErrorStats]=absor(PA,PB,...
+    'doTrans',true,...
+    'doScale',true);
 
-AT=affine3d(T);
+T = regParams.M.';  % See https://se.mathworks.com/help/images/matrix-representation-of-geometric-transformations.html
+AT = affine3d(T);
 
 
 % Register 3D volumes (loop frames)
-
-for i = 1 : size( B_dyn,4)
-    B = B_dyn(:,:,:,i);
+numberOfFrames = size( B_4D,4);
+for i = 1 : numberOfFrames
+    disp(['Registering frame = ' num2str(i) ' ( of ' num2str(numberOfFrames) ' )' ]);
+   
+    B = B_4D(:,:,:,i);
     B( isnan(B) ) = 0;  
     
     % https://se.mathworks.com/matlabcentral/answers/328737-how-can-i-do-a-homogeneous-transform-of-data-to-a-different-coordinate-system
-    [C,RC]=imwarp(B,RB,AT,'OutputView',RA); % transform into coordinate system of image B
+    [C,RC] = imwarp(B,RB,AT,'OutputView',RA); % transform into coordinate system of image B
  
     newDyn(:,:,:,i) = C;
 end
 
-imlook4d(newDyn)
+
+% DO ROIs
+    disp(['Registering ROIs ' ]);
+    B = imlook4d_ROI;
+    B( isnan(B) ) = 0;  
+    % https://se.mathworks.com/matlabcentral/answers/328737-how-can-i-do-a-homogeneous-transform-of-data-to-a-different-coordinate-system
+    [C_ROI,RC] = imwarp(B,RB,AT,'OutputView',RA); % transform into coordinate system of image B
+
+
+disp('DONE!');
+
+h = imlook4d(newDyn);
+ExportUntouched;
+imlook4d_ROI = C_ROI;
+imlook4d_ROINames = B_ROI_names;
+ImportUntouched
