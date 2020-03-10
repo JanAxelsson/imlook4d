b7a09a328d9ad585c85ba3ce4fd23d21f7c5b181
Unknown <axelsson.jan@gmail.com>
2020-03-10 18:33:03 +0100

    Improved attempt to Open .roi file if data moved

--- imlook4d/external functions/file input/lookForRoiParentFilePath.m
+++ imlook4d/external functions/file input/lookForRoiParentFilePath.m
@@ -1,6 +1,13 @@
 function newFullPath = lookForRoiParentFilePath( roiFolder, parentVolume)
 
-%roiPath = path
+% Example :
+% wrong DICOM file location :      parentVolume = C:\Users\gami0003\Desktop\NY DATA-FLTGLIOM\ID-9\ID-9 (2016-JAN-31) - 295202\<PETinGdspace>\5
+% known .roi file location  :      roiFolder =  /Users/jan/Downloads/ID-9_test-2/<PETinGdspace>
+%
+% unknown DICOM file location I want to find :  /Users/jan/Downloads/ID-9_test-2/<PETinGdspace>/5
+%
+% Common ground : <PETinGdspace>
+%
 
 
 % Search from where .roi file was open (path)
@@ -11,32 +18,62 @@ disp([ 'Trying to identify new location of parentVolume = ' parentVolume ])
 
 splitParent = split( split(parentVolume,'/'), '\'); % Allow both Windows and Mac file separators in stored string
 
-% Look for common folder in ROI-folder and and parentFolder -- follow upwards from ROI folder
-foundPath = '';
-while (length(pwd()) > 3 )
-    disp(pwd())
-    for j = 1 : length(splitParent)
-        if ( isfolder( splitParent{j}) )
-            disp(  splitParent{j} )
-            foundPath = pwd();
-            foundLevel = j;
-            break
+
+% PLAN A : Try to identify relative path from folders within same folder as ROI-file  (=ROI-folder)
+disp('Plan A : look for subfolders with correct name -- in same folder as .roi file');
+folderList = dir(roiFolder);
+firstFolder = length(splitParent)-1; % First folder - assuming last part in parentVolume is a file
+for i = 1 : length(folderList)
+    if strcmp(folderList(i).name, splitParent{firstFolder})
+        newFullPath = [roiFolder folderList(i).name filesep splitParent{firstFolder+1} ];
+        disp([ '  newFullPath = ' newFullPath ]);
+        return
+    end
+end
+
+
+% PLAN B : Try to identify relative path from ROI-folder and upwards
+% Algorithm : 
+% 1) Eat parentVolume from back until name of last folder exists in roiFolder => commonFolder
+% 2) Add part after common folder to beginning of roiFolder
+disp('Plan B : look for common folder going upwards from parentVolume path');
+j = length(splitParent);
+while ( j > 0 )
+    if contains(roiFolder,splitParent{j} )
+        commonFolderName = splitParent{j};
+        position = strfind(roiFolder,commonFolderName );
+        
+        % Path from roiFolder
+        commonFolderPath = [roiFolder(1 : position - 1) commonFolderName];
+        
+        % Build path after common folder
+        newFullPath = commonFolderPath;
+        for i = (j + 1) : length(splitParent)
+            newFullPath = [ newFullPath filesep splitParent{i} ];
+        end
+        
+        % Verify file exists and bail out
+        if isfile( newFullPath)
+            disp([ '  newFullPath = ' newFullPath ]);
+            return
+        else
+            disp([ '  newFullPath = ' newFullPath ' ( NOT A FILE )']);
+            newFullPath = '';
         end
     end
-
-    cd ..
+    j = j - 1;
 end
 
-disp([ 'Found common path = ' foundPath ])
-disp([ 'Next folder downwards = ' splitParent{foundLevel} ])
+% This point reached only if no file found
+dispRed([ 'Failed finding relative path' ]);
+dispRed([ 'Select image file manually' ]);
 
-% Build complete path
-relativeSplit =  splitParent{foundLevel:end};
-newFullPath = foundPath;
-for i = foundLevel : length(splitParent)
-    newFullPath = [newFullPath filesep splitParent{i}];
-end
+% Select file
+[file,path,indx] = uigetfile( ...
+    {'*',  'All Files'} ...
+   ,'FALLBACK - Select image file to open');
 
-disp([ 'Identified full path = ' splitParent{foundLevel} ])
+newFullPath = [path file];
+
 
 
