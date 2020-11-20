df9fbc7817d3fadbfc12255f4a3218ba523b1105
Jan Axelsson <axelsson.jan@gmail.com>
2020-11-20 15:06:44 +0100

    Fix jumping to ROI when selecting ROI

--- imlook4d/imlook4d.m
+++ imlook4d/imlook4d.m
@@ -3153,10 +3153,8 @@ function varargout = imlook4d_OutputFcn(hObject, eventdata, handles)
                         
                         [I,J,sliceWithHighestValue] = ind2sub(dims,indexToHighest);
                         if length(sliceWithHighestValue) > 1
-                            first = sliceWithHighestValue(1);
-                            last = sliceWithHighestValue(end);
-                            mid = floor( (first+last)/2);
-                            sliceWithHighestValue = sliceWithHighestValue(mid);
+                            index = round( length(sliceWithHighestValue)/2 );
+                            sliceWithHighestValue = sliceWithHighestValue( index);
                         end
                         disp(['Found ROI in slice number = ' num2str(sliceWithHighestValue) ]);
                         
